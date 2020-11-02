4d5799cdf458dcab6aed19c9ed53b61ddeca4f07
PadmaGnanapriya <padmaisuru@gmail.com>
2020-11-02 08:20:07 +0530

    Change welcome page color

---
+++ app/src/main/java/com/padme/shopping_mania/Category.kt
@@ -0,0 +1,11 @@
+package com.padme.shopping_mania
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Category : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
