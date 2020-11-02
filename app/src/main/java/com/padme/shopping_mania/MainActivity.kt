82f9acad3c83afb8c035494ef179e968f348da55
PadmaGnanapriya <padmaisuru@gmail.com>
2020-11-02 08:42:37 +0530

    make to move main activity to category activity

--- app/src/main/java/com/padme/shopping_mania/MainActivity.kt
+++ app/src/main/java/com/padme/shopping_mania/MainActivity.kt
@@ -1,11 +1,21 @@
+@file:Suppress("DEPRECATION")
+
 package com.padme.shopping_mania
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this, Category::class.java)
+            startActivity(intent)
+        }, 5000)
     }
 }
